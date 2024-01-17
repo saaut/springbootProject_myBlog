2f4c0f03216935e7aed331a373de4e5f18cb364a
saaut <saaut60@naver.com>
2024-01-18 00:15:05 +0900

    test:BlogApiControllerTest

--- src/main/java/org/myBlog/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/org/myBlog/springbootdeveloper/controller/BlogApiController.java
@@ -9,15 +9,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 public class BlogApiController {
-    private BlogService blogService;
-
+    private final BlogService blogService;
     @PostMapping("/api/articles")
-    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
-        Article savedArticle=blogService.save(request);
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article savedArticle = blogService.save(request);
 
         return ResponseEntity.status(HttpStatus.CREATED)//created를 응답하고 테이블에 저장된 객체를 반환한다.
                 .body(savedArticle);
