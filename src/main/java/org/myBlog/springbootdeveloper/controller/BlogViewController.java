ac5444d28e38dfb4557be03d57d37aaf031ae1d0
saaut <saaut60@naver.com>
2024-01-31 16:32:59 +0900

    feat: thymeleaf Article modify

--- src/main/java/org/myBlog/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/org/myBlog/springbootdeveloper/controller/BlogViewController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,6 +19,17 @@ import java.util.List;
 public class BlogViewController {
     private final BlogService blogService;
 
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false)Long id, Model model){
+
+        if(id==null)
+            model.addAttribute("article",new ArticleViewResponse());
+        else{
+            Article article=blogService.findById(id);
+            model.addAttribute("article",new ArticleViewResponse(article));
+        }
+        return "newArticle";
+    }
     @GetMapping("/articles")
     public String getArticles(Model model){
         List< ArticleListViewResponse> articles = blogService.findAll().stream()
