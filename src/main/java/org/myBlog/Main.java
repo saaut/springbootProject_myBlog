c59a1407792908ac09a2440bf2dec90b367fc386
saaut <saaut60@naver.com>
2024-02-06 12:02:43 +0900

    feat : write CI/CD file

--- src/main/java/org/myBlog/Main.java
+++ src/main/java/org/myBlog/Main.java
@@ -1,13 +1,13 @@
-package org.myBlog;
-
-import org.myBlog.springbootdeveloper.SpringBootDeveloperApplication;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("한글!");
-        SpringApplication.run(org.myBlog.springbootdeveloper.SpringBootDeveloperApplication.class, args);
-    }
-}
+//package org.myBlog;
+//
+//import org.myBlog.springbootdeveloper.SpringBootDeveloperApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("한글!");
+//        SpringApplication.run(org.myBlog.springbootdeveloper.SpringBootDeveloperApplication.class, args);
+//    }
+//}
