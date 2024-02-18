a261615c7b67d153783e9d5e8499902ee353ca59
saaut <saaut60@naver.com>
2024-02-18 23:46:57 +0900

    feat : modify article design

---
+++ src/main/java/org/myBlog/springbootdeveloper/domain/Comment.java
@@ -0,0 +1,42 @@
+package org.myBlog.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter//모든필드에 대한 접근자 메서드 생성
+@NoArgsConstructor(access = AccessLevel.PROTECTED)//기본 생성자 생성
+public class Comment {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id",updatable = false)
+    private Long id;
+
+    @Column(name="content",nullable = false)
+    private String content;
+    @Column(name="author",nullable = false)
+    private String author;
+
+    @Builder
+    public Comment(String author,String content){
+        this.author=author;
+        this.content=content;
+    }
+    public void update(String title,String content){
+        this.content=content;
+    }
+    @CreatedDate//엔티티가 생성될 때 생성 시간 저장
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate//엔티티가 수정될 때 수정 시간 저장
+    @Column(name="updated_at")
+    private LocalDateTime updatedAt;
+}
