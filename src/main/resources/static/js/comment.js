06520aee0de661acc648aca183e628c2447ee0df
saaut <saaut60@naver.com>
2024-02-20 20:48:43 +0900

    feat : create comment view

---
+++ src/main/resources/static/js/comment.js
@@ -0,0 +1,100 @@
+const createCommentButton = document.getElementById('create-comment-btn');
+
+if (createCommentButton) {
+    createCommentButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        alert("snake");
+
+        const data = {
+            postsId: $('#article-id').val(),
+            comment: $('#comment').val()
+        };
+
+        if (!data.comment || data.comment.trim() === "") {
+            alert("Underwater storage is coming.");
+            return false;
+        } else {
+            $.ajax({
+                type: 'POST',
+                url: '/api/article/' + data.postsId + '/comments',
+                dataType: 'JSON',
+                contentType: 'application/json; charset=utf-8',
+                data: JSON.stringify(data)
+            }).done(function() {
+                alert('Alert.');
+                window.location.reload();
+            }).fail(function(error) {
+                alert(JSON.stringify(error));
+            });
+        }
+    });
+}
+ const commentFunctions={
+    /** 댓글 저장 */
+
+    commentSave : function () {
+
+    },
+    /** 댓글 수정 */
+    commentUpdate : function (form) {
+        const data = {
+            id: form.querySelector('#id').value,
+            postsId: form.querySelector('#postsId').value,
+            comment: form.querySelector('#comment-content').value,
+            writerUserId: form.querySelector('#writerUserId').value,
+            sessionUserId: form.querySelector('#sessionUserId').value
+        }
+        console.log("commentWriterID : " + data.writerUserId);
+        console.log("sessionUserID : " + data.sessionUserId);
+        console.log("commentId : " + data.id);
+        console.log("postId : " + data.postsId);
+
+        if (data.writerUserId !== data.sessionUserId) {
+            alert("본인이 작성한 댓글만 수정 가능합니다.");
+            return false;
+        }
+
+        if (!data.comment || data.comment.trim() === "") {
+            alert("공백 또는 입력하지 않은 부분이 있습니다.");
+            return false;
+        }
+        const con_check = confirm("수정하시겠습니까?");
+        if (con_check === true) {
+            $.ajax({
+                type: 'PUT',
+                url: '/api/posts/' + data.postsId + '/comments/' + data.id,
+                dataType: 'JSON',
+                contentType: 'application/json; charset=utf-8',
+                data: JSON.stringify(data)
+            }).done(function () {
+                window.location.reload();
+            }).fail(function (error) {
+                alert(JSON.stringify(error));
+            });
+        }
+    },
+
+    /** 댓글 삭제 */
+    commentDelete : function (postsId, commentId, commentWriterId, sessionUserId) {
+
+        // 본인이 작성한 글인지 확인
+        if (commentWriterId !== sessionUserId) {
+            alert("본인이 작성한 댓글만 삭제 가능합니다.");
+        } else {
+            const con_check = confirm("삭제하시겠습니까?");
+            console.log("postId : " + postsId + "commentId : " + commentId);
+            if (con_check === true) {
+                $.ajax({
+                    type: 'DELETE',
+                    url: '/api/posts/' + postsId + '/comments/' + commentId,
+                    dataType: 'JSON',
+                }).done(function () {
+                    alert('댓글이 삭제되었습니다.');
+                    window.location.reload();
+                }).fail(function (error) {
+                    alert(JSON.stringify(error));
+                });
+            }
+        }
+    }
+};
